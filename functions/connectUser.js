637a428512b02779936e9c4c5effabf72caa6fc8
gcid12 <gcid12@gmail.com>
2021-08-19 12:33:10 -0700

    connectedaccount v3

--- functions/connectUser.js
+++ functions/connectUser.js
@@ -8,7 +8,7 @@ const fetchStripe =async(clientSecret, code, grantType)=>{
     const res = await fetch(`https://connect.stripe.com/oauth/token`, {
         method: 'POST',
         body: JSON.stringify({
-          clientSecret,
+          client_secret :clientSecret,
           code,
           grant_type: grantType
         })
@@ -49,49 +49,50 @@ const fetchStripe =async(clientSecret, code, grantType)=>{
 module.exports.handler = async(event, context, callback) => {
 
     //This next line is breaking the test, 
-    const requestBody = await JSON.parse(event.body);
-    const code = requestBody.code;
-    const grantType = requestBody.grantType;
-    
+    // const requestBody = await JSON.parse(event.body);
+    // const code = requestBody.code;
+
+    const code = 'ac_K3SPpGI8CdykqzBW1T18QWlmrDqRffYG';
+    const grantType = 'authorization_code';
+
 
     
     const clientSecret= process.env.STRIPE_SECRET_KEY;
 
-    return fetchStripe(clientSecret, code, grantType);
+    //return fetchStripe(clientSecret, code, grantType);
 
 
-
-    // return fetch(`https://connect.stripe.com/oauth/token`, {
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //       clientSecret,
-    //       code,
-    //       grantType
-    //     })
-    //   })
-    // .then(customer => {
-    //     const response = {
-    //         statusCode: 200,
-    //         headers: {
-    //             'Access-Control-Allow-Origin': '*',
-    //         },
-    //         body: JSON.stringify({
-    //             message: `Customer created successfully`,
-    //             customer
-    //         })
-    //     };
-    //     callback(null, response)
-    // })
-    // .catch(err => {
-    //     const response = {
-    //         statusCode: 500,
-    //         headers: {
-    //             'Access-Control-Allow-Origin': '*'
-    //         },
-    //         body: JSON.stringify({
-    //             error: err.message
-    //         })
-    //     };
-    //     callback(null, response)
-    // })
+    return fetch(`https://connect.stripe.com/oauth/token`, {
+        method: 'POST',
+        body: JSON.stringify({
+          client_secret: clientSecret,
+          code,
+          grant_type: grantType
+        })
+      })
+    .then(customer => {
+        const response = {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify({
+                message: `Connected account?`,
+                customer
+            })
+        };
+        callback(null, response)
+    })
+    .catch(err => {
+        const response = {
+            statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                error: err.message
+            })
+        };
+        callback(null, response)
+    })
 }
