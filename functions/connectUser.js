08c9dcc1da4e5bb06970d853a083e0951939258d
gcid12 <gcid12@gmail.com>
2021-08-19 12:31:15 -0700

    connectuser v2

--- functions/connectUser.js
+++ functions/connectUser.js
@@ -1,71 +1,97 @@
 // ROUTE: /connectUser
-//AddCard.js:76
+//ConnectAccount.js:91
 
-module.exports.handler = (event, context, callback) => {
-    const requestBody = JSON.parse(event.body);
+const fetch = require('node-fetch')
+
+const fetchStripe =async(clientSecret, code, grantType)=>{
+
+    const res = await fetch(`https://connect.stripe.com/oauth/token`, {
+        method: 'POST',
+        body: JSON.stringify({
+          clientSecret,
+          code,
+          grant_type: grantType
+        })
+      });
+    const data = await res.json()
+
+    const response2 = {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({
+            message: `Customer connected Succesfully`,
+            data
+        })
+    };
+
+    if (data.message && data.message.includes('success')) {
+        
+        return response2;
+    }else{
+        
+        const errorresponse = {
+                    statusCode: 500,
+                    headers: {
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    body: JSON.stringify({
+                        error: err.message
+                    })
+        };
+                //callback(null, response)
+        return errorresponse;
+    }
+
+}
+
+module.exports.handler = async(event, context, callback) => {
+
+    //This next line is breaking the test, 
+    const requestBody = await JSON.parse(event.body);
     const code = requestBody.code;
     const grantType = requestBody.grantType;
+    
+
+    
     const clientSecret= process.env.STRIPE_SECRET_KEY;
 
-    // const res = await fetch(`https://connect.stripe.com/oauth/token`, {
+    return fetchStripe(clientSecret, code, grantType);
+
+
+
+    // return fetch(`https://connect.stripe.com/oauth/token`, {
     //     method: 'POST',
     //     body: JSON.stringify({
     //       clientSecret,
     //       code,
     //       grantType
     //     })
-    //   });
-    // const data = await res.json()
-    // const response2 = {
-    //     statusCode: 200,
-    //     headers: {
-    //         'Access-Control-Allow-Origin': '*',
-    //     },
-    //     body: JSON.stringify({
-    //         message: `Customer connected Succesfully`,
-    //         data
-    //     })
-    // };
-
-    // if (data.message && data.message.includes('success')) {
-    //     callback(null, response2)
-    //     return data;
-    // }else{
-    //     return null
-    // }
-
-
-    return fetch(`https://connect.stripe.com/oauth/token`, {
-        method: 'POST',
-        body: JSON.stringify({
-          clientSecret,
-          code,
-          grantType
-        })
-      })
-    .then(customer => {
-        const response = {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify({
-                message: `Customer created successfully`,
-                customer
-            })
-        };
-        callback(null, response)
-    })
-    .catch(err => {
-        const response = {
-            statusCode: 500,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify({
-                error: err.message
-            })
-        };
-        callback(null, response)
-    })
+    //   })
+    // .then(customer => {
+    //     const response = {
+    //         statusCode: 200,
+    //         headers: {
+    //             'Access-Control-Allow-Origin': '*',
+    //         },
+    //         body: JSON.stringify({
+    //             message: `Customer created successfully`,
+    //             customer
+    //         })
+    //     };
+    //     callback(null, response)
+    // })
+    // .catch(err => {
+    //     const response = {
+    //         statusCode: 500,
+    //         headers: {
+    //             'Access-Control-Allow-Origin': '*'
+    //         },
+    //         body: JSON.stringify({
+    //             error: err.message
+    //         })
+    //     };
+    //     callback(null, response)
+    // })
 }
